f65ca52056c9209579cad72e7484441b109e6c54
serp-ya <sergei.kundryukov1989@gmail.com>
2017-06-04 06:24:46 +0300

    For watching

---
+++ alma/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.slider-full').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    dots: false
+  });
+});
